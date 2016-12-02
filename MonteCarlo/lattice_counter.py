06f9f55509a05b57c2e599947b9c654f20e78657
Mike Chapman <mikechapman979@gmail.com>
2016-12-02 15:15:25 -0500

    Actually added lattice_counter.py this time

---
+++ MonteCarlo/lattice_counter.py
@@ -0,0 +1,26 @@
+form os import listdir
+
+def lattice_counter(dim, L):
+	if L > 9:
+		filenames = listdir("../data/TC-D%i-L%i-Raw/" % (dim, L))
+	else:
+		filenames = listdir("../data/TC-D%i-L0%i-Raw/" % (dim, L))
+
+	temps = []
+	for filename in filenames:
+		new_temp = filename[1:6]
+		new_temp.replace(",", ".")
+		new_temp = int(new_temp)
+		included = False
+		for temp in temps:
+			if temp['temp'] == new_temp:
+				temp['count'] += 1
+				included = True
+				break
+		if not included:
+			temps.append({'temp': new_temp, 'count': 1})
+
+	for temp in temps:
+		print str(temp['temp']) + " " + str(temp["count"])
+
+lattice_counter(3,12)
